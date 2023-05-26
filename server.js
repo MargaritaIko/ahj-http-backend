7f6f487a3bc9d92a52276e8ddbf0782b12718caa
Margarita May Lee <ritochka.m_lee@yahoo.com>
2023-05-26 19:06:59 +0300

    Remake 1.0

--- server.js
+++ server.js
@@ -1,52 +1,27 @@
 const http = require('http');
 const Koa = require('koa');
 const koaBody = require('koa-body');
+const app = new Koa();
 const uuid = require('uuid');
 
-const app = new Koa();
-
 const port = process.env.PORT || 7070;
+const server = http.createServer(app.callback()).listen(port)
+//http.createServer(app.callback()).listen(port)
 
-const tickets = [
-  {
-    id: 'first',
-    name: 'Ticket with description',
-    description: 'Remote connection issue',
-    status: 'in progress',
-    created: new Date().toLocaleString(),
-  },
-  {
-    id: 'second',
-    name: 'Ticket without description',
-    description: '',
-    status: 'fixed',
-    created: new Date().toLocaleString(),
-  },
-];
-
-app.use(
-  koaBody({
-    text: true,
-    urlencoded: true,
-    multipart: true,
-    json: true,
-  }),
-);
-
+// => CORS
 app.use(async (ctx, next) => {
-  const origin = ctx.request.get('Origin');  
+  const origin = ctx.request.get('Origin');
   if (!origin) {
     return await next();
   }
-
-  const headers = { 'Access-Control-Allow-Origin': '*' };
+  const headers = { 'Access-Control-Allow-Origin': '*', };
 
   if (ctx.request.method !== 'OPTIONS') {
-    ctx.response.set({ ...headers });
+    ctx.response.set({...headers});
     try {
       return await next();
     } catch (e) {
-      e.headers = { ...e.headers, ...headers };
+      e.headers = {...e.headers, ...headers};
       throw e;
     }
   }
@@ -58,61 +33,88 @@ app.use(async (ctx, next) => {
     });
 
     if (ctx.request.get('Access-Control-Request-Headers')) {
-      ctx.response.set('Access-Control-Allow-Headers', ctx.request.get('Access-Control-Request-Headers'),);
+      ctx.response.set('Access-Control-Allow-Headers', ctx.request.get('Access-Control-Request-Headers'));
     }
-
     ctx.response.status = 204;
   }
 });
+  
+// => Body Parsers
+app.use(koaBody({
+  text: true,
+  urlencoded: true,
+  multipart: true,
+  json: true,
+}));
+
+let tickets = [];
+class Tickets {
+  constructor(id, name, description, status, created) {
+    this.id = id;
+    this.name = name;
+    this.description = description;
+    this.status = status;
+    this.created = created;
+  }
+}
+
+// kod
 
 app.use(async (ctx) => {
-  ctx.response.body = `server response at port ${port}`;
-  const { method } = ctx.request.query;
-  switch (method) {
-    case 'allTickets':
-      ctx.response.body = tickets;
+  // GET
+  if (ctx.method === 'GET') {
+    const { id } = ctx.request.query;
+    if (id) {
+      const ticket = tickets.find(item => item.id == id);
+      ctx.response.body = ticket.description;
       return;
-    case 'ticketById':
-      const ticketById = tickets.find((ticket) => ticket.id === ctx.request.query.id);
-      const ticketDescription = ticketById.description;
-      ctx.response.body = ticketDescription;
-      return;
-    case 'addTicket':
-      const newTicketId = uuid.v4()
-      const formData = ctx.request.body;
-      formData.id = newTicketId;
-      if (formData.status === 'in progress') formData.status = false;
-      if (formData.status === 'fixed') formData.status = true;
-      tickets.push(formData);
-      return;
-    case 'changeTicketStatus':
-      const ticketId = tickets.find((ticket) => ticket.id === ctx.request.body.id);
-      if (!ticketId) return;
-      ticketId.status = ctx.request.body.status;
-      return;
-    case 'removeTicket':
-      tickets = tickets.filter((ticket) => ticket.id !== ctx.request.body.id);
-      return;
-    case 'editTicket':
-      const
-        {
-          id,
-          name,
-          description,
-          status,
-          created,
-        } = ctx.request.body;
-      const editingTicket = tickets.find((ticket) => ticket.id === id);
-      if (!editingTicket) return;
-      editingTicket.name = name;
-      editingTicket.description = description;
-      editingTicket.status = status;
-      editingTicket.created = created;
-      return;
-    default:
-      ctx.response.status = 404;
+    }
+    ctx.response.body = tickets.map((item) => {
+      return {
+        id: item.id,
+        name: item.name,
+        status: item.status,
+        created: item.created,
+      };
+    });
+    return;
+  }
+
+  // POST
+  if (ctx.method === 'POST') {
+    const { name, description } = ctx.request.body;
+    const id = uuid.v4();
+    const created = new Date();
+    tickets.push(new Tickets(id, name, description, false, created));
+    ctx.response.body = tickets;
+    return;
+  };
+
+  // PUT
+  if (ctx.method === 'PUT') {
+    const { id, name, description } = ctx.request.body;
+    const index = tickets.findIndex((item) => item.id === id);
+    console.log(index);
+    tickets[index].name = name;
+    tickets[index].description = description;
+    ctx.response.body = 'ok';
+    return;
+  }
+
+  // PATCH
+  if (ctx.method === 'PATCH') {
+    const { id, status } = ctx.request.query;
+    const index = tickets.findIndex((item) => item.id === id);
+    tickets[index].status = status;
+    ctx.response.body = 'ok';
+    return;
+  }
+
+  // DELETE
+  if (ctx.method === 'DELETE') {
+    const { id } = ctx.request.query;
+    tickets = tickets.filter((item) => item.id !== id);
+    ctx.response.body = 'ok';
+    return;
   }
 });
-
-app.listen(port, () => console.log(`Koa server has been started on port ${port} ...`));
-// http.createServer(app.callback()).listen(process.env.PORT || 7070);
