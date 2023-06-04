de935a28fb79c9677cee98c4e8bed04ab5bc5278
Margarita May Lee <ritochka.m_lee@yahoo.com>
2023-06-04 18:14:20 +0300

    Some Fixes

--- server.js
+++ server.js
@@ -6,115 +6,129 @@ const uuid = require('uuid');
 
 const port = process.env.PORT || 7070;
 const server = http.createServer(app.callback()).listen(port)
+server.listen(port, (err) => {
+  if (err) {
+    return console.log('Error occured:', error);
+  }
+  console.log(`server is listening on ${port}`);
+});
 //http.createServer(app.callback()).listen(port)
 
-// => CORS
-app.use(async (ctx, next) => {
-  const origin = ctx.request.get('Origin');
-  if (!origin) {
-    return await next();
-  }
-  const headers = { 'Access-Control-Allow-Origin': '*', };
+app.use(
+  koaBody({
+    text: true,
+    urlencoded: true,
+    multipart: true,
+    json: true,
+  })
+);
 
-  if (ctx.request.method !== 'OPTIONS') {
-    ctx.response.set({...headers});
-    try {
-      return await next();
-    } catch (e) {
-      e.headers = {...e.headers, ...headers};
-      throw e;
-    }
-  }
-
-  if (ctx.request.get('Access-Control-Request-Method')) {
-    ctx.response.set({
-      ...headers,
-      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH',
-    });
-
-    if (ctx.request.get('Access-Control-Request-Headers')) {
-      ctx.response.set('Access-Control-Allow-Headers', ctx.request.get('Access-Control-Request-Headers'));
-    }
-    ctx.response.status = 204;
-  }
-});
-  
-// => Body Parsers
-app.use(koaBody({
-  text: true,
-  urlencoded: true,
-  multipart: true,
-  json: true,
-}));
+app.use(koaCors());
 
 let tickets = [];
-class Tickets {
-  constructor(id, name, description, status, created) {
-    this.id = id;
-    this.name = name;
-    this.description = description;
-    this.status = status;
-    this.created = created;
+
+//GET
+app.use(async (ctx, next) => {
+  if (ctx.request.method !== 'GET' || !ctx.request.url.startsWith('/tickets')) {
+    next();
+    return;
   }
-}
 
-// kod
+  try {
+    const { method, id } = ctx.query;
 
-app.use(async (ctx) => {
-  // GET
-  if (ctx.method === 'GET') {
-    const { id } = ctx.request.query;
-    if (id) {
-      const ticket = tickets.find(item => item.id == id);
-      ctx.response.body = ticket.description;
-      return;
+    switch (method) {
+      case 'allTickets':
+        ctx.response.body = tickets.map(({ description, ...rest }) => rest);
+        break;
+      case 'ticketById':
+        const foundTicket = tickets.find((el) => el.id == id);
+        if (foundTicket) {
+          ctx.response.body = foundTicket;
+        }
+        break;
     }
-    ctx.response.body = tickets.map((item) => {
-      return {
-        id: item.id,
-        name: item.name,
-        status: item.status,
-        created: item.created,
-      };
-    });
-    return;
-  }
-
-  // POST
-  if (ctx.method === 'POST') {
-    const { name, description } = ctx.request.body;
-    const id = uuid.v4();
-    const created = new Date();
-    tickets.push(new Tickets(id, name, description, false, created));
-    ctx.response.body = tickets;
-    return;
-  };
-
-  // PUT
-  if (ctx.method === 'PUT') {
-    const { id, name, description } = ctx.request.body;
-    const index = tickets.findIndex((item) => item.id === id);
-    console.log(index);
-    tickets[index].name = name;
-    tickets[index].description = description;
-    ctx.response.body = 'ok';
-    return;
-  }
-
-  // PATCH
-  if (ctx.method === 'PATCH') {
-    const { id, status } = ctx.request.query;
-    const index = tickets.findIndex((item) => item.id === id);
-    tickets[index].status = status;
-    ctx.response.body = 'ok';
-    return;
-  }
-
-  // DELETE
-  if (ctx.method === 'DELETE') {
-    const { id } = ctx.request.query;
-    tickets = tickets.filter((item) => item.id !== id);
-    ctx.response.body = 'ok';
+  } catch (error) {
+    ctx.response.status = 500;
     return;
   }
 });
+
+//DELETE
+app.use(async (ctx, next) => {
+  if (ctx.request.method !== 'DELETE' || !ctx.request.url.startsWith('/tickets')) {
+    next();
+    return;
+  }
+
+  try {
+    const { method, id } = ctx.query;
+
+    if (method === 'deleteById') {
+      tickets = tickets.filter((el) => el.id != id);
+      ctx.response.status = 204;
+    }
+  } catch (error) {
+    ctx.response.status = 500;
+    return;
+  }
+});
+
+//POST
+app.use(async (ctx, next) => {
+  if (ctx.request.method !== 'POST' || !ctx.request.url.startsWith('/tickets')) {
+    next();
+    return;
+  }
+
+  try {
+    const { method } = ctx.query;
+
+    if (method === 'createTicket') {
+      const { body } = ctx.request;
+
+      body.id = uuid.v4();
+      body.status = false;
+      body.created = Date.now();
+
+      tickets.push(body);
+      ctx.response.body = body;
+    }
+  } catch (error) {
+    ctx.response.status = 500;
+    return;
+  }
+});
+
+//PUT
+app.use(async (ctx, next) => {
+  if (ctx.request.method !== 'PUT' || !ctx.request.url.startsWith('/tickets')) {
+    next();
+    return;
+  }
+
+  try {
+    const { method, id } = ctx.query;
+
+    if (method === 'updateById') {
+      const { body } = ctx.request;
+
+      const idx = tickets.findIndex((el) => el.id == id);
+
+      if (idx < 0) {
+        return;
+      }
+
+      tickets[idx].name = body.hasOwnProperty('name') ? body.name : tickets[idx].name;
+      tickets[idx].status = body.hasOwnProperty('status') ? body.status : tickets[idx].status;
+      tickets[idx].description = body.hasOwnProperty('description') ? body.description : tickets[idx].description;
+
+      ctx.response.body = tickets[idx];
+    }
+  } catch (error) {
+    ctx.response.status = 500;
+    return;
+  }
+});
+
+module.exports = app;
